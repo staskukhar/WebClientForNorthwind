492a4591cfce1c0bc7d5a8fb94e2dd2a24729f7d
staskukhar <108075412+staskukhar@users.noreply.github.com>
2023-02-11 20:04:26 +0200

    start working with asp.net mvc

---
+++ Northwind.Mvc/Program.cs
@@ -0,0 +1,47 @@
+// Section 1 - import namespaces
+using Microsoft.AspNetCore.Identity; // Indentity users
+using Microsoft.EntityFrameworkCore; // For Sqlite or MS SQL SERVER
+using Northwind.Mvc.Data; //DB Context
+// Other imports - obj\Debug\net7.0\Northwind.Mvc.GlobalUsings.g.cs.
+
+// Section 2 - configure the host web server including services
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Section 3 - Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+// Section 4 - start the host web server listening for HTTP requests
+app.Run(); // blocking call
